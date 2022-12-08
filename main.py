7ba0395dc14fb81f9381b0a3cc7868e09dccbb2b
S F <91188115+BSM-MrF@users.noreply.github.com>
2022-12-08 16:49:38 +0000

    init. commit

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import sqlite3
+
+# Connect to db
 connection = sqlite3.connect("zoo.db")
+
+# test connection
+print(connection.total_changes)
+
+# to execute sql commands we need a cursor object
+cursor = connection.cursor()
+
